cfead29ff127116cb8f80cf11c6c56639738d326
Ankush Shandil <ankushshandil@apptunix.com>
2025-07-05 12:46:01 +0530

    Initial Commit

---
+++ JobHunt/JobHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobHuntApp.swift
+//  JobHunt
+//
+//  Created by Ankush Shandil on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct JobHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
